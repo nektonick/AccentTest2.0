bf48c99d72efc0a9a4b50398a5f885181af4e383
nektonick <nik-1905@mail.ru>
2020-08-02 23:57:09 +0300

    Main test(AccentTest class) done. Style for buttons added.

--- AccentTest.cpp
+++ AccentTest.cpp
@@ -5,7 +5,7 @@
 AccentTest::AccentTest(QWidget *parent) : QWidget(parent), ui(new Ui::AccentTest){
     ui->setupUi(this);
     wordsVector.getWordsByReadingFromFileOrCreatingDefaultFile();
-    this->showWord();
+    this->showRandomWord();
 }
 
 
@@ -13,13 +13,13 @@ AccentTest::~AccentTest(){
     delete ui;
 }
 
-void AccentTest::showWord(){
+void AccentTest::showRandomWord(){
     word = wordsVector.getGoodRandomWord();
     buttons.generateButtonsFor(word);
     int vowels = 0;
     for (int i=0; i<buttons.size(); ++i){
-         ui->buttonsLayout->addWidget(&buttons[i]);
-        if (isVowel( buttons[i].text())){
+        ui->buttonsLayout->addWidget(&buttons[i]);
+        if (ButtonsWithLetter::isVowel(buttons[i].text()[0])){
             vowels++;
             if (vowels == word.accentLetterNumber){
                 connect(&buttons[i], SIGNAL(clicked()), this, SLOT(rightVowelClick()));
@@ -30,20 +30,65 @@ void AccentTest::showWord(){
         }
     }
 }
-
-bool AccentTest::isVowel(QString s)
+void AccentTest::wrongAnswerTextAdd()
 {
-    QString vowels ="аеёиоуыэюя";
-    return vowels.contains(s);
+    QString s = word.getText();
+    s[userAnswer.toInt()] = ButtonsWithLetter::makeLetterBig(s[userAnswer.toInt()]);
+    wrongAnswerLabel.setText("<s> "+ s + "</s");
+    setRedLableStyleSheet(wrongAnswerLabel);
+    ui->wrongAnswerTextLayout->addWidget(&wrongAnswerLabel);
+    wrongAnswerLabel.show();
+}
+
+void AccentTest::setGreenButtonStyleSheet(QPushButton &b)
+{
+    b.setStyleSheet(
+                "color:green; font-size:24px;"
+                "font-weight:bold; border-radius:10%;"
+                "padding: 5px;"
+                "margin:3px; border:3px solid green;"
+                );
+}
+
+void AccentTest::setRedLableStyleSheet(QLabel &l)
+{
+    l.setAlignment(Qt::AlignHCenter);
+    l.setStyleSheet(
+                "color:red; font-size: 18px;"
+                "font-weight: normal;"
+                "padding: 1px;"
+                "margin:0px; border:0px"
+                );
 }
 
 void AccentTest::rightVowelClick()
 {
-    //qDebug()<<"rightAccent"<<endl;
     buttons.deleteAllButtons();
+    buttons.generateAnswer(word);
+    //кнопка только одна, имеет индекс [0]
+    setGreenButtonStyleSheet(buttons[0]);
+    ui->buttonsLayout->addWidget(&buttons[0]);
+    connect(&buttons[0], SIGNAL(clicked()), this, SLOT(nextWordClick()));
 }
 
 void AccentTest::wrongVowelClick()
 {
-    //qDebug()<<"wrongAccent"<<endl;
+    userAnswer=this->sender()->objectName();
+
+    buttons.deleteAllButtons();
+    buttons.generateAnswer(word);
+    wrongAnswerTextAdd();
+    //кнопка только одна, имеет индекс [0]
+    setGreenButtonStyleSheet(buttons[0]);
+    ui->buttonsLayout->addWidget(&buttons[0]);
+    connect(&buttons[0], SIGNAL(clicked()), this, SLOT(nextWordClick()));
+}
+
+void AccentTest::nextWordClick()
+{
+    buttons.deleteAllButtons();
+    if (wrongAnswerLabel.text() !="" && wrongAnswerLabel.text() !="TextLabel"){
+        wrongAnswerLabel.hide();
+    }
+    this->showRandomWord();
 }
--- ButtonsWithLetter.h
+++ ButtonsWithLetter.h
@@ -12,9 +12,14 @@ public:
     ~ButtonsWithLetter();
     std::vector<QPushButton*> buttonsWithLetter;
     void generateButtonsFor(WordWithAccentAndStatistic word);
+    void generateAnswer(WordWithAccentAndStatistic word);
+    QString wordWithRightAccent(WordWithAccentAndStatistic word);
     void deleteAllButtons();
     int size();
     QPushButton& operator [] (int index);
+    static QChar makeLetterBig(QCharRef c);
+    static bool isVowel(QCharRef s);
+
 };
 
 #endif // BUTTONSWITHLETTER_H
